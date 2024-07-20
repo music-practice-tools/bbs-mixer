f781742d4026ad05bbd4cbdf937b03a7e3d73d92
Steve Lee <steve@opendirective.com>
2024-07-20 18:42:57 +0100

    remove dead code

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,5 @@
 export function load({ params }) {
     return {
-        mixer: true,
         title: 'BBS Mixer',
         nav: [{ type: 'filepicker', label: 'Load Folder' }, { href: '/faq', label: 'FAQ' }],
     }
